3c413efea2c9a180d0b56c1d9e40dc2acae77755
Josh <joshzuniga@gmail.com>
2022-05-28 17:36:21 +0000

    First

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long arrayManipulation(int n, List<List<int>> queries)
+    {
+      long ret = 1000;
+      return ret;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = args[0].Split(' ');
+     
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]) + 1;
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 1; i < m; i++)
+        {
+            queries.Add(args[i].
+            TrimEnd()
+            .Split(' ')
+            .ToList()
+            .Select(queriesTemp => Convert.ToInt32(queriesTemp))
+            .ToList());
+        }   
+
+        long result = Result.arrayManipulation(n, queries);
+    }
+}
